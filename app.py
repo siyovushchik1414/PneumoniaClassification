dac21ccfa27fa84a1f8b2bd71e4d6bac550459aa
Hamidov Siyovush <93854666+siyovushchik1414@users.noreply.github.com>
2023-02-25 19:21:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from matplotlib.patches import Rectangle
 from keras.models import load_model
 import cv2
 
-NeuralNetwork = load_model('D:\Desktop\PneumoniaClassification/PneuClass(90%).h5')
+NeuralNetwork = load_model('PneuClass(90%).h5')
 img_size = 150
   
 
